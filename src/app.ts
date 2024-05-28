b553e9f52f7eb445f41ea9e9049e05971339f128
mhasan0505 <mhasan.coder@gmail.com>
2024-05-29 01:45:31 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+// import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+// import notFound from "./app/middlewares/notFound";
+// import router from "./app/routes";
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+
+
+const test = (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);  
+};
+
+app.get("/", test);
+
+
+
+//Not Found
+
+export default app;
